1335d839a1cfa2f277e3d555cc0c18fc400e4296
CoderX <66735676+ToxygenX@users.noreply.github.com>
2024-02-25 22:57:30 +0330

    Update message.py

--- telethon/tl/custom/message.py
+++ telethon/tl/custom/message.py
@@ -6,8 +6,29 @@ from .messagebutton import MessageButton
 from .forward import Forward
 from .file import File
 from .. import TLObject, types, functions, alltlobjects
+from ..extensions import markdown
 from ... import utils, errors
 
+# Support for spoiler and custom emoji 
+class CustomMarkdown:
+    @staticmethod
+    def parse(text):
+        text, entities = markdown.parse(text)
+        for i, e in enumerate(entities):
+            if isinstance(e, types.MessageEntityTextUrl):
+                if e.url == 'spoiler':
+                    entities[i] = types.MessageEntitySpoiler(e.offset, e.length)
+                elif e.url.startswith('emoji/'):
+                    entities[i] = types.MessageEntityCustomEmoji(e.offset, e.length, int(e.url.split('/')[1]))
+        return text, entities
+    @staticmethod
+    def unparse(text, entities):
+        for i, e in enumerate(entities or []):
+            if isinstance(e, types.MessageEntityCustomEmoji):
+                entities[i] = types.MessageEntityTextUrl(e.offset, e.length, f'emoji/{e.document_id}')
+            if isinstance(e, types.MessageEntitySpoiler):
+                entities[i] = types.MessageEntityTextUrl(e.offset, e.length, 'spoiler')
+        return markdown.unparse(text, entities)
 
 # TODO Figure out a way to have the code generator error on missing fields
 # Maybe parsing the init function alone if that's possible.
